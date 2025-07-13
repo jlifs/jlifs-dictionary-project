675d774bd5c98d8830af6bb647953fdb6fb6030f
jlifs <jlifs74@gmail.com>
2025-07-13 14:48:50 -0400

    Updated component

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,20 +6,17 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index)
-      {
-      return (
-        <div key={index}>
-          <p>
-            <strong>Definition:</strong> {definition. definition}
-            <br />
-            <strong>Example:</strong>
-            <em>{definition.example}</em>
-            <Synonyms synonyms={definition.synonyms} />
-          </p>
-        </div>
-      );
-    })}
+      <div>
+        <p>
+          <strong>Definition:</strong> {props.meaning.definition}
+          <br />
+          <strong>Example:</strong>
+          <em>{props.meaning.example}</em>
+          <br />
+          <strong>Synonym:</strong>
+          <em>{props.meaning.synonym}</em>
+        </p>
+      </div>
     </div>
   );
 }
