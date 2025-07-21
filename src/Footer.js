c88a57ac0de07230a3d5ae3d480231145f264747
jlifs <jlifs74@gmail.com>
2025-07-21 14:58:36 -0400

    Added to footer

--- src/Footer.js
+++ src/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
         target="_blank"
         rel="noopener noreferrer"
       >
-        Jennifer Lifsey
+        Jennifer
       </a>{" "}
       and is open-sourced on{" "}
       <a
