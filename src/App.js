49f464486479c95eee08797bea50b5bbca156497
jlifs <jlifs74@gmail.com>
2025-07-21 14:07:32 -0400

    Added logo image and more styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import logo from "./logo.png";
+import "./App.css"
 import Dictionary from "./Dictionary";
 import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
-      <Dictionary defaultKeyword="sunset" />
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary defaultKeyword="sunset" />
+        </main>
       <Footer />
     </div>
+  </div>
   );
 }
